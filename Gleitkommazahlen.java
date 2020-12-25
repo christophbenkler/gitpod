d742a49cd69f8957ef2b74d9e9a981f296106043
christophbenkler <christoph.benkler@me.com>
2020-12-25 12:20:59 +0100

    update

--- Gleitkommazahlen.java
+++
@@ -1,9 +0,0 @@
-public class Gleitkommazahlen {
-    public static void main(String[] args){
-
-        double x = 0.1 + 0.1 + 0.1;
-        System.out.println(x == 0.3); // false
-        System.out.println(Double.compare(x, 0.3)); // 1/-1
-
-    }
-}
--- Hello.java
+++ Hello.java
@@ -1,15 +1,5 @@
 public class Hello {
     public static void main(String[] args){
-
-        // Das ist ein Kommentar
-
-        /*
-        Das ist ein
-        zeilenübergreifender
-        Kommentar
-        */
-
-        // Die folgende Zeile gibt «Hello» aus:
         System.out.println("Hello");
     }
 }
